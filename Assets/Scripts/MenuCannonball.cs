a0b070a5d17cfb510685f186bd0ad3fd9c3f3c6e
1mpulseee <83034758+1mpulseee@users.noreply.github.com>
2024-11-28 15:19:47 +0300

    Fix multi menu open

--- Assets/Scripts/MenuCannonball.cs
+++ Assets/Scripts/MenuCannonball.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 public class MenuCannonball : MonoBehaviour
 {
+    bool Use = false;
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "MenuButton")
+        if (collision.gameObject.tag == "MenuButton" && !Use)
         {
+            Use = true;
             collision.gameObject.GetComponentInParent<MenuButton>().Click(gameObject.transform.position);
         }
     }

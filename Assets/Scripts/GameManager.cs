98fe5bd5e341936533dde9ac309f5ca8eaebfec6
1mpulseee <83034758+1mpulseee@users.noreply.github.com>
2024-12-01 15:59:02 +0300

    WinScreen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,24 +15,35 @@ public class GameManager : MonoBehaviour
         set
         {
             _TotalDest = value;
-            LevelCompletionBar.fillAmount = (float)value / ((float)buildsCount * LevelCompletionPercent);
+            float FillValue = (float)value / ((float)buildsCount * LevelCompletionPercent);
+            LevelCompletionBar.fillAmount = FillValue;
+            if (FillValue > 1f)
+            {
+                Invoke("Win", 1f);
+            }
             Debug.Log((float)value / ((float)buildsCount * LevelCompletionPercent));
         }
     }
     int _TotalDest;
     [SerializeField][Range(0f, 1f)] float LevelCompletionPercent = .75f;
     [SerializeField] Image LevelCompletionBar;
+    [SerializeField] GameObject WinScreen;
     private void Awake()
     {
         instance = this;
         builds = GetComponentsInChildren<DestructionBuild>();
         buildsCount = builds.Length;
         TotalDest = 0;
+        WinScreen.SetActive(false);
         Invoke("StartGame", 2f);
     }
     public void StartGame()
     {
         GameStarted = true;
+    }
+    public void Win()
+    {
+        WinScreen.SetActive(true);
     }
     public void BuildingDestroyed() => TotalDest++;
 }

d7d2c7e5ec628a0bec504205d748d41cf1f0238e
1mpulseee <83034758+1mpulseee@users.noreply.github.com>
2024-12-01 20:02:28 +0300

    Sound

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,6 +3,24 @@ public class Menu : MonoBehaviour
 {
     [SerializeField] GameObject cannonball;
     [SerializeField] Transform RifleStart;
+    bool SoundIsActive = true;
+    [SerializeField] GameObject SoundOn, SoundOff;
+    public void SoundChange()
+    {
+        SoundIsActive = !SoundIsActive;
+        if (SoundIsActive)
+        {
+            AudioListener.volume = 0.1f;
+            SoundOn.SetActive(true);
+            SoundOff.SetActive(false);
+        }
+        else
+        {
+            AudioListener.volume = 0.0f;
+            SoundOn.SetActive(false);
+            SoundOff.SetActive(true);
+        }
+    }
     void Update()
     {
         if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
